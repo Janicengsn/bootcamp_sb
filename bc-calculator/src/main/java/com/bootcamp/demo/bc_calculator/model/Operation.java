c3009670dd7c830641c2d0955d62eea8f4d71925
janicengsn <janicensn0610@gmail.com>
2025-07-19 15:45:52 +0800

    Add files via upload

---
+++ bc-calculator/src/main/java/com/bootcamp/demo/bc_calculator/model/Operation.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demo.bc_calculator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Operation {
+  private String add;
+  private String sub;
+  private String mul;
+  private String div;
+}
